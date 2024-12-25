d3992b0a0586e6f19892382e2610a6eeb0e86cbf
Kevin <bkevin93@hotmail.com>
2024-12-25 19:13:37 +0000

    aaa

--- pi2.com/scripts/build.sh
+++ pi2.com/scripts/build.sh
@@ -40,7 +40,7 @@ message 1 "Running apt-get  update and upgrade"
 apt-get update && apt-get upgrade -y
 
 ## Install required packages without prompt if they don't exist, update if they do
-packages=(bash wget curl net-tools inotify-tools apache2 zip unzip gzip imagemagick virt-what) 
+packages=(bash wget curl net-tools inotify-tools apache2 zip gzip tar unzip gzip imagemagick virt-what psmisc less) 
 
 for package in "${packages[@]}"; do
         message 1 "Installing $package"
@@ -66,6 +66,8 @@ else
         message 3 "Unsupported hypervisor '$virt'"
         exit 1
 fi
+
+mkdir -p $mountpoint
 
 # Create the file /usr/bin/root which contains 'sudo bash'
 message 1 "Creating /usr/bin/root"
@@ -95,9 +97,10 @@ else
 fi
 
 # Add /etc/smbcredentials
-username=Kevin
-password=Daisy12345
+
 if [ ! -f "/etc/smbcredentials" ]; then
+        username=Kevin
+        password=Daisy12345
         message 1 "Creating /etc/smbcredentials"
         echo -e "username=$username\npassword=$password" | sudo tee /etc/smbcredentials > /dev/null
         sudo chmod 600 /etc/smbcredentials
@@ -109,9 +112,97 @@ fi
 message 1 "Mounting all in fstab"
 mount -a
 
-# Check if /mnt/hgfs/shared exists
-if [ ! -d "/mnt/hgfs/shared" ]; then
-        message 3 "Shared folder /mnt/hgfs/shared does not exist"
+# Check /mnt/shared exists
+message 1 "Checking if /mnt/shared exists"
+if [ -d "/mnt/shared" ]; then
+        message 1 "/mnt/shared exists"
+else
+        message 3 "/mnt/shared does not exist"
+        exit 1
+fi
+
+# Get latest shared folder date
+latest=$(ls -t /mnt/shared/backups/daily/ | head -n1 | head -n1)
+message 1 "Latest shared folder is $latest"
+
+# Check apps.zip exists in latest shared folder
+message 1 "Checking if apps.zip exists in $latest"
+if [ -f "/mnt/shared/backups/daily/$latest/apps.zip" ]; then
+        message 1 "apps.zip exists in $latest"
+else
+        message 3 "apps.zip does not exist in $latest"
+        exit 1
+fi
+
+# Move /apps and /web to today's date
+message 1 "Moving /apps and /web to $(date +%Y_%m_%d)"
+mv /apps /apps_$(date +%Y_%m_%d)
+mv /web /web_$(date +%Y_%m_%d)
+
+# Move /etc/apache2 to /etc/apache2_$(date +%Y_%m_%d)
+message 1 "Moving /etc/apache2 to /etc/apache2_$(date +%Y_%m_%d)"
+mv /etc/apache2 /etc/apache2_$(date +%Y_%m_%d)
+
+# Unzip apps.zip and web.zip to /apps and /web
+message 1 "Unzipping apps.zip and web.zip to /apps and /web"
+unzip /mnt/shared/backups/daily/$latest/apps.zip -d /
+unzip /mnt/shared/backups/daily/$latest/web.zip -d /
+
+# Unzip etcapache.zip to /
+message 1 "Unzipping etcapache.zip to /"
+unzip /mnt/shared/backups/daily/$latest/etcapache.zip -d /
+
+# Remove /usr/lib/cgi-bin and repoint to /apps/html/cgi
+message 1 "Removing /usr/lib/cgi-bin and repointing to /apps/html/cgi"
+rm -rf /usr/lib/cgi-bin
+ln -s /apps/html/cgi /usr/lib/cgi-bin
+
+# Create the user apache if it doesn't already exist
+message 1 "Creating user apache"
+if id apache &>/dev/null; then
+        message 1 "User apache already exists"
+else
+        message 1 "User apache does not exist, creating"
+        # Create /home/apache if it doesn't exist
+        if [ ! -d "/home/apache" ]; then
+                message 1 "Creating /home/apache"
+                mkdir /home/apache
+        fi
+
+        # Copy /etc/skel/.bashrc and /etc/skel/.profile to /home/apache
+        message 1 "Copying /etc/skel/.bashrc and /etc/skel/.profile to /home/apache"
+        cp /etc/skel/.bashrc /home/apache
+        cp /etc/skel/.profile /home/apache
+
+        # Create user apache, set home directory to /home/apache, login shell to /bin/bash
+        message 1 "Creating user apache"
+        useradd -d /home/apache -s /bin/bash apache
+
+        # Set group of apache to users
+        message 1 "Setting group of apache to users"
+        usermod -g users apache
+        usermod -aG apache apache
+
+        # Set password for apache
+        message 1 "Setting password for apache"
+        echo "apache:apache" | chpasswd
+fi
+
+
+# Update ownership of /apps, /web, /logs and /etc/apache2 to apache
+message 1 "Updating ownership of /apps, /web, /logs and /etc/apache2 to apache"
+chown -R apache:users /apps /web /logs /etc/apache2
+
+# Restart apache
+message 1 "Restarting apache"
+systemctl restart apache2
+
+# Check if localhost/cgi/gallery/mini is reachable
+message 1 "Checking if localhost/cgi/gallery/mini is reachable"
+if curl -s "http://localhost/cgi/gallery/mini/" | grep -q "clean_generated_files"; then
+        message 1 "http://localhost/cgi/gallery/mini/ is reachable"
+else
+        message 3 "http://localhost/cgi/gallery/mini/ is not reachable"
         exit 1
 fi
 
